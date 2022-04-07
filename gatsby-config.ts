df5ead6fbcc06d8af9138a25fa4d772ad0aad9e6
zhengjinyang <zhengjinyang@heytea.com>
2022-04-07 11:12:26 +0800

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `xyang-log`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
