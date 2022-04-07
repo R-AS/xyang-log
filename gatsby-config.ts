689f551bd1ab5f6a20bbe7c95ad720487616f716
zhengjinyang <zhengjinyang@heytea.com>
2022-04-07 18:45:46 +0800

    feat: 搭建环境

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,24 @@
 import type { GatsbyConfig } from "gatsby"
+import { resolve } from 'path'
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `xyang-log`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: resolve('src/blog/'),
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 }
 
 export default config
